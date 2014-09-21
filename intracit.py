8bf9c9f285d0a3172b17d56eb9502194b0452805
Ege Demirel <egedemirel@gmail.com>
2014-09-21 06:36:37 +0000

    Long Time No Commit

--- intracit.py
+++ intracit.py
@@ -26,5 +26,11 @@ def sup():
 
     return render_template('index.html')
 
+
+@app.route("/gmapstest/")
+def gsup():
+
+    return render_template('gmaps_test.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
